1f8cc484a9055c60f93ded4bd9b578053b5f863e
Midenlex Joseph <midenlex.joseph@gmail.com>
2017-10-29 03:23:48 -0400

    Update synch.h

--- kern/include/synch.h
+++ kern/include/synch.h
@@ -47,10 +47,10 @@ struct semaphore {
         char *sem_name;
 	struct wchan *sem_wchan;
 	struct spinlock sem_lock;
-        volatile unsigned sem_count;
+        volatile int sem_count;
 };
 
-struct semaphore *sem_create(const char *name, unsigned initial_count);
+struct semaphore *sem_create(const char *name, int initial_count);
 void sem_destroy(struct semaphore *);
 
 /*
@@ -74,13 +74,13 @@ void V(struct semaphore *);
  */
 struct lock {
         char *lk_name;
-        HANGMAN_LOCKABLE(lk_hangman);   /* Deadlock detector hook. */
-        // add what you need here
-        // (don't forget to mark things volatile as needed)
+	struct wchan *lk_wchan;
+	struct spinlock lk_lock;
+	struct thread *volatile lk_holder;
 };
 
 struct lock *lock_create(const char *name);
-void lock_destroy(struct lock *);
+void lock_acquire(struct lock *);
 
 /*
  * Operations:
@@ -88,14 +88,14 @@ void lock_destroy(struct lock *);
  *                   same time.
  *    lock_release - Free the lock. Only the thread holding the lock may do
  *                   this.
- *    lock_do_i_hold - Return true if the current thread holds the lock;
+ *    lock_do_i_hold - Return true if the current thread holds the lock; 
  *                   false otherwise.
  *
  * These operations must be atomic. You get to write them.
  */
-void lock_acquire(struct lock *);
 void lock_release(struct lock *);
 bool lock_do_i_hold(struct lock *);
+void lock_destroy(struct lock *);
 
 
 /*
@@ -114,8 +114,7 @@ bool lock_do_i_hold(struct lock *);
 
 struct cv {
         char *cv_name;
-        // add what you need here
-        // (don't forget to mark things volatile as needed)
+	struct wchan *cv_wchan;
 };
 
 struct cv *cv_create(const char *name);
@@ -128,7 +127,7 @@ void cv_destroy(struct cv *);
  *    cv_signal    - Wake up one thread that's sleeping on this CV.
  *    cv_broadcast - Wake up all threads sleeping on this CV.
  *
- * For all three operations, the current thread must hold the lock passed
+ * For all three operations, the current thread must hold the lock passed 
  * in. Note that under normal circumstances the same lock should be used
  * on all operations with any particular CV.
  *
